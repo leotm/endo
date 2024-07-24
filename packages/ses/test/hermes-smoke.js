a5b11b3b84a7f33323b0f0177f30cc62a1e2805b
LeoTM <1881059+leotm@users.noreply.github.com>
2024-07-24 18:03:48 +0100

    test(ses): add hermes smoke test

---
+++ packages/ses/test/hermes-smoke.js
@@ -0,0 +1,42 @@
+import '../dist/ses.cjs';
+import '../src/assert-shim.js';
+
+// Test lockdown
+
+lockdown();
+
+// Test Compartment
+
+const c = new Compartment();
+
+c.evaluate('1+1');
+c.evaluate("const c2 = new Compartment(); c2.evaluate('1+2')");
+
+// Test importHook and resolveHook (non-ESM)
+
+// https://github.com/facebook/hermes/blob/main/doc/Features.md
+// In Progress: ES modules (`import` and `export`)
+
+const resolveHook = '';
+
+const importHook = async () => {
+  return {
+    imports: [],
+    exports: ['meaning'],
+    execute(exports) {
+      exports.meaning = 42;
+    },
+  };
+};
+
+const compartment = new Compartment({}, {}, { resolveHook, importHook });
+
+const module = compartment.module('.');
+
+const {
+  namespace: { meaning },
+} = await compartment.import('.');
+
+assert(module);
+// t.is(meaning, 42, 'exports seen');
+// t.is(module.meaning, 42, 'exports seen through deferred proxy');
