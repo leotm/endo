5d4a919a4b7f90389bbf43a06182aa5b8784cb2d
LeoTM <1881059+leotm@users.noreply.github.com>
2024-07-31 16:02:25 +0100

    test(ses): create shellscript to run hermesc on any OS

---
+++ packages/ses/scripts/hermesc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+OS="$(uname -s)"
+
+case "$OS" in
+    Linux*)
+        OS_DIR="linux64-bin"
+        ;;
+    Darwin*)
+        OS_DIR="osx-bin"
+        ;;
+    CYGWIN*|MINGW*|MSYS*)
+        OS_DIR="win64-bin"
+        ;;
+    *)
+        echo "Unsupported OS: $OS"
+        exit 1
+        ;;
+esac
+
+HERMESC="../../node_modules/hermes-engine-cli/$OS_DIR/hermesc"
+
+$HERMESC -emit-binary -out test/hermes-smoke.hbc test/hermes-smoke.js
